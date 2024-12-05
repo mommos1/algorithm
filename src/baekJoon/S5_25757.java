e23e15e2ae7e3d98a45dd0019c766ab997d5ec3a
mommos <gjwnsqja45@naver.com>
2024-12-05 18:02:41 +0900

    S5 25757

--- src/baekJoon/S5_25757.java
+++ src/baekJoon/S5_25757.java
@@ -1,4 +1,22 @@
 package baekJoon;
 
+import java.io.*;
+import java.util.*;
+
 public class S5_25757 {
+    public static void main(String[] aa) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        String game = st.nextToken();
+
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+
+
+        }
+    }
 }
