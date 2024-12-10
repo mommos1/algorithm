5bc23aed776c0bd69dc2561392d569f27a6cdec7
mommos <gjwnsqja45@naver.com>
2024-12-10 17:59:41 +0900

    S5 25757

--- src/baekJoon/S5_25757.java
+++ src/baekJoon/S5_25757.java
@@ -8,15 +8,43 @@ public class S5_25757 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Y", 2);
+        map.put("F", 3);
+        map.put("O", 4);
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         String game = st.nextToken();
 
-        String[] arr = new String[n];
+        List<String> arr = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             String s = br.readLine();
-
-
+            arr.add(s);
         }
+
+        int max = map.get(game);
+        List<String> chk = new ArrayList<>();
+        int count = 1;
+        int answer = 0;
+
+        for (String name : arr) {
+            if (chk.contains(name)) {
+                continue;
+            }
+            else {
+                count++;
+                chk.add(name);
+            }
+
+            if (count == max) {
+                answer++;
+                count = 1;
+            }
+        }
+
+        bw.write(String.valueOf(answer));
+        bw.flush();
+        bw.close();
     }
 }
